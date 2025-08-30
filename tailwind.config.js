ec6f30a58a937a868b7e92692b103766cf6c0087
lorelairaras <lorelairaras@gmail.com>
2025-08-31 00:02:18 +0800

    Made minor changes to the file paths and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "hsl(0, 100%, 100%)",
+        black: "hsl(0, 0%, 0%)",
+        "grey-500": "hsl(0, 0%, 63%)",
+        "grey-800": "hsl(0, 0%, 27%)",
+      },
+    },
   },
   plugins: [],
 };
